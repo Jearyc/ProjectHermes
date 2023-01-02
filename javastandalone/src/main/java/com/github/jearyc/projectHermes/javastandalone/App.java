b1624c85eab3b93328e57254c188c6cc66df9db5
rjric <rjrice_94@yahoo.com>
2023-01-01 22:39:41 -0500

    First IDE Commit

---
+++ javastandalone/src/main/java/com/github/jearyc/projectHermes/javastandalone/App.java
@@ -0,0 +1,8 @@
+package com.github.jearyc.projectHermes.javastandalone;
+
+public class App {
+    
+    public static void main(String[] args) {
+    }
+    
+}
---
+++ javastandalone/src/test/java/com/github/jearyc/projectHermes/javastandalone/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.jearyc.projectHermes.javastandalone;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+
+}
